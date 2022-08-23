88fc12cb0a34e24ed9094e6d696842ebde5d9d63
DELL <DELL@DESKTOP-3N8I2RQ>
2022-08-23 23:16:30 +0530

    This is project 4

---
+++ Sort Array/src/Fruits.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+public class Fruits {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String fruits[]= new String[] {"apple","mango","banana","cherry"}; //declaration and initialization of an array
+		Arrays.sort(fruits);//sort an array
+		for(String x:fruits)
+		{
+			System.out.println(x);// print the name of fruits
+			
+		}
+		
+
+	}
+
+}
