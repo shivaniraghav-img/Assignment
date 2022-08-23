7630ba35163e0e4c65a7f89995f6f186a1e8db92
DELL <DELL@DESKTOP-3N8I2RQ>
2022-08-23 23:19:57 +0530

    This is project 5

---
+++ Indexing/src/Index.java
@@ -0,0 +1,17 @@
+
+public class Index {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a[]= {75,4,56,5,36,100,77,18,29,94};//declaration and initialization
+		for(int i=0;i<a.length;i++) {
+		if(a[i]==100)//check the index position of 100
+		{
+			System.out.println("Index of 100 position is:"+ a[i]);
+		 }
+	   }
+    }
+  }
+	
+
+	
